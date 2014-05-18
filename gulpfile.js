fd0ca78fcc1b36def01cc9c2326e53ef67d77993
yoshuawuyts <i@yoshuawuyts.com>
2014-05-18 13:13:43 +0200

    Update support files

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,29 @@
  * Module dependencies
  */
 
-var browserify = require('gulp-browserify');
-var rename = require('gulp-rename');
+var source = require('vinyl-source-stream');
+var livereload = require('gulp-livereload');
+var browserify = require('browserify');
 var concat = require('gulp-concat');
 var myth = require('gulp-myth');
 var gulp = require('gulp');
 
 /**
  * Compile CSS
+ *
+ * Includes css files in the following order
+ * /base/reset.css > /base/vars.css > /base/*.css > all other css files.
  */
 
 gulp.task('styles', function() {
   gulp
-    .src('client/styles/*.css')
-    .pipe(myth())
+    .src(['client/modules/index/reset.css',
+      'client/modules/index/vars.css',
+      'client/modules/index/!(reset, vars)*.css', 
+      'client/modules/!(index)**/*.css'
+    ])
     .pipe(concat('build.css'))
+    .pipe(myth())
     .pipe(gulp.dest('build'));
 });
 
@@ -27,11 +35,35 @@ gulp.task('styles', function() {
  */
 
 gulp.task('modules', function() {
-  gulp
-    .src('client/modules/index.js')
-    .pipe(browserify({buffer: false, debug: true}))
-    .pipe(rename('build.js'))
+  browserify('./client/modules/index/index.js')
+    .bundle({debug: true})
+    .pipe(source('build.js'))
     .pipe(gulp.dest('build/'));
+});
+
+/**
+ * Copy assets
+ */
+
+gulp.task('assets', function() {
+  gulp
+    .src(['client/modules/index/*.ttf'])
+    .pipe(gulp.dest('build/fonts/'));
+});
+
+/**
+ * Watch for file changes
+ */
+
+gulp.task('watch', function() {
+  var reload = function(file) {
+    livereload().changed(file.path);
+  };
+
+  gulp.watch(['client/**/*.js', 'client/**/**/*.js', 'client/**/**/**/*.js'], ['modules']);
+  gulp.watch('client/modules/**/*.css', ['styles']);
+  gulp.watch('client/modules/**/*.tff', ['assets']);
+  gulp.watch(['build/**']).on('change', reload);
 });
 
 /**
@@ -40,5 +72,17 @@ gulp.task('modules', function() {
 
 gulp.task('default', [
   'styles',
-  'modules'
+  'modules',
+  'assets',
+  'watch'
+]);
+
+/**
+ * Deploy
+ */
+
+gulp.task('deploy', [
+  'styles',
+  'modules',
+  'assets'
 ]);
